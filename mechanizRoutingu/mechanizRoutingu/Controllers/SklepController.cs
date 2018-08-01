9f8d9a54003f15c923a673e2dd0d4a92b1a8d2d1
Jakub47 <jakub7447@gmail.com>
2018-08-01 23:28:06 +0200

    First projects

---
+++ mechanizRoutingu/mechanizRoutingu/Controllers/SklepController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mechanizRoutingu.Controllers
+{
+    [RoutePrefix("AtrybutPrefix")]
+    public class SklepController : Controller
+    {
+        [Route("atrybut/routing/{parametr}/trasa")]
+        public string Index(string parametr)
+        {
+            return "Definiowanie tras - Atrybuty";
+        }
+
+        public string Test(string parametr)
+        {
+            return parametr;
+        }
+    }
+}
