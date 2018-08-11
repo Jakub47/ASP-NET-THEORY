f2f8e850a5abc773a5f62445f0e81afd83033928
Jakub47 <jakub7447@gmail.com>
2018-08-11 18:08:22 +0200

    h

---
+++ Kontrolery/Kontrolery/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kontrolery.MvcApplication" Language="C#" %>
---
+++ Kontrolery/Kontrolery/Models/Ksiazka.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kontrolery.Models
+{
+    public class Ksiazka
+    {
+        public string Tytul { get; set; }
+        public string Autor { get; set; }
+        public decimal Cena { get; set; }
+    }
+}
