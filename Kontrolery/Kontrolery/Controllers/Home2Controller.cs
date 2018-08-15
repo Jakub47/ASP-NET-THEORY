8f52d9b7ca504f83e8e34c8aec5271c50cc9899d
Jakub47 <jakub7447@gmail.com>
2018-08-15 09:21:21 +0200

    p

---
+++ Kontrolery/Kontrolery/Controllers/Home2Controller.cs
@@ -0,0 +1,26 @@
+﻿using Kontrolery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kontrolery.Controllers
+{
+    public class Home2Controller : Controller
+    {
+        //[NonAction]
+        //[ActionName("Edycja")]
+        [HttpGet]
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Ksiazka k)
+        {
+            return View();
+        }
+    }
+}
