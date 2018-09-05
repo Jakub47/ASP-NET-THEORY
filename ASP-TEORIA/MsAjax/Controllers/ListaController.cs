0106f4618e3e649ec1d5abe6550aa015fe84db5a
Jakub47 <jakub7447@gmail.com>
2018-09-05 19:22:09 +0200

    Microsoft AAjax

---
+++ ASP-TEORIA/MsAjax/Controllers/ListaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MsAjax.Controllers
+{
+    public class ListaController : Controller
+    {
+        static List<string> listaElementow = new List<string>();
+
+        // GET: Lista
+        public ActionResult Index()
+        {
+            ViewBag.Lista = new SelectList(listaElementow);
+            return View();
+        }
+
+        public ActionResult DodajElementDoListy(FormCollection formCollection)
+        {
+            listaElementow.Add(formCollection[0].ToString());
+            return RedirectToAction("Index");
+        }
+    }
+}
