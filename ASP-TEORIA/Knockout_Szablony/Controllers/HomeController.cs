ad7ca8716d0eaf9f9f89afb7cc30ab626e00e0ac
Jakub47 <jakub7447@gmail.com>
2018-09-15 22:22:35 +0200

    Szablony

---
+++ ASP-TEORIA/Knockout_Szablony/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Knockout_Szablony.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Knockout_Szablony.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var produkty = new[]
+            {
+                new Produkt {Nazwa = "Produkt1", DataDodania = "2010-01-01"},
+                new Produkt {Nazwa = "Produkt2", DataDodania = "2011-05-10"},
+                new Produkt {Nazwa = "Produkt3", DataDodania = "2009-02-03"}
+            };
+            return View(produkty);
+        }
+    }
+}
---
+++ ASP-TEORIA/Knockout_Szablony/Models/Produkt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Knockout_Szablony.Models
+{
+    public class Produkt
+    {
+        public string Nazwa { get; set; }
+        public string DataDodania { get; set; }
+    }
+}
