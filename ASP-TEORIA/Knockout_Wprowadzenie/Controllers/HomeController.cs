5417e98780b43a4b47f9f4af4aaa66284464241b
Jakub47 <jakub7447@gmail.com>
2018-09-15 22:11:59 +0200

    Knockout-1

---
+++ ASP-TEORIA/Knockout_Wprowadzenie/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Knockout_Wprowadzenie.Models;
+
+
+namespace Knockout_Wprowadzenie.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var osoba = new Osoba { Imie = "Maciej", Nazwisko = "pakulski" };
+            return View(osoba);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ASP-TEORIA/Knockout_Wprowadzenie/Models/Osoba.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Knockout_Wprowadzenie.Models
+{
+    public class Osoba
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+    }
+}
